4b8b4bf5003dc915311625455ccf184071afeab3
sortiz0640 <s.ortizvargas0302@gmail.com>
2025-07-10 10:34:43 -0600

    Agrego validaciones en formularios de sebas

---
+++ EmprendimientoFormulario/validacion-emprendimiento.js
@@ -0,0 +1,122 @@
+
+// Obtener elementos del DOM
+const inputs = document.querySelectorAll('#formularioEmprendimiento input');
+const textarea = document.querySelectorAll('#formularioEmprendimiento textarea');
+const selectArea = document.querySelectorAll('#formularioEmprendimiento select')
+
+const formulario = document.getElementById("formularioEmprendimiento");
+const boton = document.querySelector("#boton-submit");
+
+// Expresiones regulares
+const expresiones = {
+    nombreNegocio: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{5,50}$/, 
+    telefono: /^\d{4}-\d{4}$/,
+    ubicacion: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s\.,#\-]{10,100}$/, 
+    descripcion: /^[\s\S]{20,500}$/,
+    estado: /^(?!Estado$).+$/, 
+    categoria: /^(?!Categoría$).+$/
+}
+
+// Estado de los campos
+const campos = {
+    nombreNegocio: false,
+    telefono: false,
+    ubicacion: false,
+    descripcion: false,
+    estado: false,
+    categoria: false
+}
+
+// Función principal de validación
+const validarFormulario = (e) => {
+    const nombreCampo = e.target.name;
+    
+    switch(nombreCampo) {
+        case "nombreNegocio":
+        case "telefono":
+        case "ubicacion":
+        case "descripcion":
+        case "estado":
+        case "categoria":
+            validarCampo(expresiones[nombreCampo], e.target, nombreCampo);
+            break;
+    }
+}
+
+// Función para validar campo individual
+const validarCampo = (expresion, input, campo) => {
+    const grupo = document.querySelector(`#grupo-${campo}`);
+    const mensajeError = grupo.querySelector('.formulario-input-error');
+    const textArea = grupo.querySelector('.area-texto');
+    const selectArea = grupo.querySelector('.seleccion-formulario');
+    
+    if(expresion.test(input.value)) {
+        // Validación correcta
+        input.classList.remove("campo-incorrecto");
+        input.classList.add("campo-correcto");
+        mensajeError.classList.remove("input-error-activo");
+
+        // Solo aplica a textareas si existen
+        if(textArea) {
+            textArea.classList.remove("campo-incorrecto");
+            textArea.classList.add("campo-correcto");
+        }
+
+        if (selectArea) {
+            selectArea.classList.remove('campo-incorrecto');
+            selectArea.classList.add('campo-correcto')
+        }
+
+        campos[campo] = true;
+    } else {
+        // Validación incorrecta
+        input.classList.add("campo-incorrecto");
+        input.classList.remove("campo-correcto");
+        mensajeError.classList.add("input-error-activo");
+
+        // Solo aplica a textareas si existen
+        if(textArea) {
+            textArea.classList.add("campo-incorrecto");
+            textArea.classList.remove("campo-correcto");
+        }
+
+        if (selectArea) {
+            selectArea.classList.remove("campo-incorrecto");
+            selectArea.classList.add('campo-correcto');
+        }
+
+        campos[campo] = false;
+    }
+
+    verificarCampos();
+}
+
+const verificarCampos = () => {
+    if (campos.nombreNegocio && campos.categoria && campos.descripcion && campos.telefono && campos.ubicacion && campos.estado) {
+        boton.disabled = false;
+        boton.classList.remove('boton-deshabilitado')
+    } else {
+        boton.disabled = true; 
+        boton.classList.add('boton-deshabilitado')
+    }
+}
+
+
+
+// Llamar la funcion ayuda a que el boton salga como deshabilitado al cargar la pagina.
+verificarCampos();
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+});
+
+textarea.forEach((textarea) => {
+    textarea.addEventListener("keyup", validarFormulario);
+    textarea.addEventListener("blur", validarFormulario);
+})
+
+selectArea.forEach((selectArea) => {
+    selectArea.addEventListener("keyup", validarFormulario);
+    selectArea.addEventListener("blur", validarFormulario);
+})
---
+++ OfertasFormulario/validacion-ofertas.js
@@ -0,0 +1,120 @@
+
+// Obtener elementos del DOM
+const inputs = document.querySelectorAll('#formularioOfertas input');
+const textarea = document.querySelectorAll('#formularioOfertas textarea');
+const selectArea = document.querySelectorAll('#formularioOfertas select')
+
+const formulario = document.getElementById("formularioOfertas");
+const boton = document.querySelector("#boton-submit");
+
+// Expresiones regulares
+const expresiones = {
+    nombre: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{5,50}$/, 
+    descripcion: /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s\.,#\-]{10,100}$/, 
+    articulo: /^(?!Articulo$).+$/,
+    fechaInicio: /^(?!DD\/MM\/YYYY$).+$/, 
+    fechaFin: /^(?!DD\/MM\/YYYY$).+$/
+    
+}
+
+// Estado de los campos
+const campos = {
+    articulo: false,
+    nombre: false,
+    descripcion: false,
+    fechaInicio: false,
+    fechaFin: false
+}
+
+// Función principal de validación
+const validarFormulario = (e) => {
+    const nombreCampo = e.target.name;
+    
+    switch(nombreCampo) {
+        case "nombre":
+        case "descripcion":
+        case "fechaInicio":
+        case "fechaFin":
+        case "articulo":
+            validarCampo(expresiones[nombreCampo], e.target, nombreCampo);
+            break;
+    }
+}
+
+// Función para validar campo individual
+const validarCampo = (expresion, input, campo) => {
+    const grupo = document.querySelector(`#grupo-${campo}`);
+    const mensajeError = grupo.querySelector('.formulario-input-error');
+    const textArea = grupo.querySelector('.area-texto');
+    const selectArea = grupo.querySelector('.seleccion-formulario');
+    
+    if(expresion.test(input.value)) {
+        // Validación correcta
+        input.classList.remove("campo-incorrecto");
+        input.classList.add("campo-correcto");
+        mensajeError.classList.remove("input-error-activo");
+
+        // Solo aplica a textareas si existen
+        if(textArea) {
+            textArea.classList.remove("campo-incorrecto");
+            textArea.classList.add("campo-correcto");
+        }
+
+        if (selectArea) {
+            selectArea.classList.remove('campo-incorrecto');
+            selectArea.classList.add('campo-correcto')
+        }
+
+        campos[campo] = true;
+    } else {
+        // Validación incorrecta
+        input.classList.add("campo-incorrecto");
+        input.classList.remove("campo-correcto");
+        mensajeError.classList.add("input-error-activo");
+
+        // Solo aplica a textareas si existen
+        if(textArea) {
+            textArea.classList.add("campo-incorrecto");
+            textArea.classList.remove("campo-correcto");
+        }
+
+        if (selectArea) {
+            selectArea.classList.remove("campo-incorrecto");
+            selectArea.classList.add('campo-correcto');
+        }
+
+        campos[campo] = false;
+    }
+
+    verificarCampos();
+}
+
+const verificarCampos = () => {
+    if (campos.nombre && campos.articulo && campos.descripcion && campos.fechaInicio && campos.fechaFin) {
+        boton.disabled = false;
+        boton.classList.remove('boton-deshabilitado')
+    } else {
+        boton.disabled = true; 
+        boton.classList.add('boton-deshabilitado')
+    }
+}
+
+
+
+// Llamar la funcion ayuda a que el boton salga como deshabilitado al cargar la pagina.
+verificarCampos();
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", validarFormulario);
+    input.addEventListener("blur", validarFormulario);
+});
+
+textarea.forEach((textarea) => {
+    textarea.addEventListener("keyup", validarFormulario);
+    textarea.addEventListener("blur", validarFormulario);
+})
+
+selectArea.forEach((selectArea) => {
+    selectArea.addEventListener("keyup", validarFormulario);
+    selectArea.addEventListener("blur", validarFormulario);
+})
