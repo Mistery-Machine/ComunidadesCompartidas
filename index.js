c24725eafae0ccd0f15bf17cb1e0d493465751aa
sortiz0640 <s.ortizvargas0302@gmail.com>
2025-07-29 12:10:03 -0600

    Implementacion EJS navbar - footer

--- index.js
+++ index.js
@@ -22,5 +22,98 @@ app.listen(3000, () => {
 
 // RUTAS
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("index");
 });
+
+app.get("/anuncios", (req, res) => {
+  res.render("anuncios");
+});
+
+app.get("/calendario", (req, res) => {
+  res.render("calendario");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+app.get("/emprendimientos", (req, res) => {
+  res.render("emprendimientos");
+});
+
+app.get("/eventos", (req, res) => {
+  res.render("eventos");
+});
+
+app.get("/formulario-anuncio", (req, res) => {
+  res.render("formulario-anuncio");
+});
+
+app.get("/formulario-eventos", (req, res) => {
+  res.render("formulario-eventos");
+});
+
+app.get("/formulario-emprendimiento", (req, res) => {
+  res.render("formularioEmprendimiento");
+});
+
+app.get("/formulario-ofertas", (req, res) => {
+  res.render("formularioOfertas");
+});
+
+app.get("/formulario-ruta", (req, res) => {
+  res.render("formularioRuta");
+});
+
+app.get("/listado-rutas", (req, res) => {
+  res.render("listadoRutas");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/mapa", (req, res) => {
+  res.render("mapa");
+});
+
+app.get("/pagina-eventos", (req, res) => {
+  res.render("paginaEventos");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/reportes", (req, res) => {
+  res.render("reportes");
+});
+
+app.get("/ruta", (req, res) => {
+  res.render("ruta");
+});
+
+app.get("/ver-reportes", (req, res) => {
+  res.render("verReportes");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
